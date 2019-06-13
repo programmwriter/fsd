87674c43abb22688815165c1e1051211f5afbec6
programmwriter <programmwriter@mail.ru>
2019-06-13 16:40:52 +0500

    test2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ let template = require("./users.pug");
 let locals = {
   users: ["user1", "user2", "user3", "user4", "user5"]
 };
-
-document.querySelector("main").innerHTML = template(locals);
-console.log(bro("bratka"));
+let users = ["user1", "user2", "user3", "user4", "user5"];
+//document.querySelector("main").innerHTML = template(locals);
