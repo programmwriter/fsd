789063df5fba575047a819af8ae39c5d3cc4ceb3
programmwriter <programmwriter@mail.ru>
2019-08-22 18:24:08 +0500

    dropdown in progress 9

--- src/pages/ui-kit/form_elements/dropdown_ncs/dropdownNcs.js
+++ src/pages/ui-kit/form_elements/dropdown_ncs/dropdownNcs.js
@@ -1,6 +1,11 @@
+//Плагин dropdown NCS, изначально плагин был рассчитан для одного экземпляра на странице.
+//Данная реализация  применяется к коллекци.
+//Опции для каждого отдельно экзэмпляра беруться из атрибута Data
+
 $(document).ready(function() {
   (function($) {
     $.fn.NCS = function(options) {
+      //Конфигурация плагина по умолчани.
       var settings = $.extend(
         {
           // Defaults.
@@ -22,20 +27,23 @@ $(document).ready(function() {
         options
       );
 
+      //основная функция которая применяется к каждому элементу коллекции
       function main(elem) {
-        // это основная функция
         $input = elem;
         $inputContainer = elem.parent();
         $originalPlaceholder = $input.attr("placeholder");
+
+        //Опции из атрибута data-opts
         const options = $input.data().opts;
 
+        //Мержим оции поумолчанию и оции из атрибута data-opt
         for (item in settings) {
           if (!options.hasOwnProperty(item)) {
             options[item] = settings[item];
           }
         }
-        console.log(options);
 
+        //Если не заданны значения поумолчанию, то заполняем массив значений нулями
         if (!options.categoryValues) {
           options.categoryValues = newFilledArray(
             options.categoryNames.length,
@@ -43,38 +51,47 @@ $(document).ready(function() {
           );
         }
 
+        //создаем dom самого выпадающего списка
         $parent = createHTML();
 
+        //если задаана опция закрытия выпадающего списка по клику снаружи блока
         if (options.closeOnOutsideClick) {
-          $(document).mouseup(function(e) {
-            if (
-              !$input.is(e.target) &&
-              !$parent.is(e.target) &&
-              $parent.has(e.target).length === 0 &&
-              !$("div.NCS.display").is(e.target) &&
-              $("div.NCS.display").has(e.target).length === 0
-            ) {
-              if (options.fade) {
-                $parent.fadeOut(200);
-              } else {
-                $parent.hide();
+          $(document).mouseup(function(event) {
+            $("input[name='NCS']").each(function() {
+              $input = $(this);
+              $parent = $input.next();
+              if (
+                !$input.is(event.target) &&
+                !$parent.is(event.target) &&
+                $parent.has(event.target).length === 0 &&
+                !$("div.NCS.display").is(event.target) &&
+                $("div.NCS.display").has(event.target).length === 0
+              ) {
+                if (options.fade) {
+                  $parent.fadeOut(200);
+                } else {
+                  $parent.hide();
+                }
+                if ($input.hasClass("ncs_clicked")) {
+                  $input.removeClass("ncs_clicked");
+                }
               }
-              if ($($input).hasClass("ncs_clicked")) {
-                $($input).removeClass("ncs_clicked");
-              }
-            }
+            });
           });
         }
 
+        //открываем выпадающий список по клику на input, а так же делаем его активным
         $input.click(function() {
           switchSelector($(this));
           switchSelectorInput($(this));
         });
 
+        //изменение положения выпадающего списка при ресайзе окна
         $(window).resize(function() {
           setPositions();
         });
 
+        //инициализация колбэка
         function doCallback() {
           if (typeof options.callback == "function") {
             var callbackResult = {};
@@ -86,6 +103,7 @@ $(document).ready(function() {
           }
         }
 
+        //функция изменения позиции
         function setPositions() {
           switch (options.align) {
             case "left":
@@ -128,8 +146,13 @@ $(document).ready(function() {
             $display.css("height", $input.height() - 1);
           }
         }
-
-        $parent.find("a.NCS.button.plus").click(function() {
+        //увеличение значения при нажатии на кнопку +
+        $parent.find("a.NCS.button.plus").click(function(e) {
+          e.preventDefault();
+          $parent = $(e.target)
+            .parent()
+            .parent()
+            .parent();
           $category = $(this).attr("category");
           if (options.categoryValues[$category] < options.maxValue) {
             options.categoryValues[$category]++;
@@ -155,7 +178,7 @@ $(document).ready(function() {
           }
           if (options.showText) {
             if (!options.useDisplay) {
-              updateText();
+              updateText($parent);
             } else {
               updateElement();
             }
@@ -163,7 +186,13 @@ $(document).ready(function() {
           return false;
         });
 
-        $parent.find("a.NCS.button.minus").click(function() {
+        //уменьшение значения при нажатии на кнопку -
+        $parent.find("a.NCS.button.minus").click(function(e) {
+          e.preventDefault();
+          $parent = $(e.target)
+            .parent()
+            .parent()
+            .parent();
           $category = $(this).attr("category");
           if (options.categoryValues[$category] > options.minValue) {
             options.categoryValues[$category]--;
@@ -189,7 +218,7 @@ $(document).ready(function() {
           }
           if (options.showText) {
             if (!options.useDisplay) {
-              updateText();
+              updateText($parent);
             } else {
               updateElement();
             }
@@ -197,13 +226,14 @@ $(document).ready(function() {
           return false;
         });
 
-        function clearValues() {
-          $values = $parent("div.NCS.value").each(function() {
+        //Очищаеть значения в dom дереве и делает кнопку - неактивной
+        function clearValues(parent) {
+          $values = parent.find("div.NCS.value").each(function() {
             $value = $(this);
             $value.text(options.categoryValues[$value.attr("category")]);
           });
 
-          $parent("a.NCS.button.minus").addClass("inactive");
+          parent.find("a.NCS.button.minus").addClass("inactive");
         }
 
         function updateElement() {
@@ -230,7 +260,12 @@ $(document).ready(function() {
           updateText();
         }
 
-        function updateText() {
+        //функция обновления знечений в инпуте
+        function updateText(parent) {
+          console.log(options.categoryValues);
+          if (parent) {
+            $input = $(parent).prev();
+          }
           $text = "";
           $added = 0;
           if (options.clearAndCloseButtons) {
@@ -259,6 +294,7 @@ $(document).ready(function() {
           $input.val($text);
         }
 
+        //функция создания dom дерева выпадающего писка
         function createHTML() {
           $input.attr("type", "text");
           if (options.useDisplay) {
@@ -354,13 +390,17 @@ $(document).ready(function() {
             );
             $clear.click(function(e) {
               e.preventDefault();
+              $parent = $(e.target)
+                .parent()
+                .parent();
+
               options.categoryValues = newFilledArray(
                 options.categoryNames.length,
                 0
               );
-              updateText();
-              clearValues();
-              $;
+              updateText($parent);
+              clearValues($parent);
+              // $;
             });
             $confirm = $(
               "<a class='NCS__confirm' href=''>применить</a>"
@@ -391,8 +431,8 @@ $(document).ready(function() {
           return $parent;
         }
 
+        //функция появления и скрытия выпадающего списка
         function switchSelector(input) {
-          // console.log(input);
           if (options.fade) {
             input.next().fadeToggle(200);
           } else {
@@ -400,10 +440,11 @@ $(document).ready(function() {
           }
         }
 
+        // пункция переключения инпута в активное или неактивное состояние
         function switchSelectorInput(input) {
           input.toggleClass("ncs_clicked");
         }
-
+        //функция заполнения массива значений нулями
         function newFilledArray(len, val) {
           var rv = new Array(len);
           while (--len >= 0) {
@@ -413,6 +454,7 @@ $(document).ready(function() {
         }
       }
 
+      //применение основной функции на все экземпляры коллекции
       this.each(function() {
         main($(this));
       });
@@ -420,6 +462,7 @@ $(document).ready(function() {
     };
   })(jQuery);
 
+  //функции склонения значений в зависимости от количества
   function num2str(n, text_forms) {
     n = Math.abs(n) % 100;
     var n1 = n % 10;
@@ -439,6 +482,7 @@ $(document).ready(function() {
     return text_forms[2];
   }
 
+  //инициализация плагина на всю коллекцию Dropdown
   $("input[name='NCS']").NCS({
     categoryNames: ["взрослые", "дети", "младенцы"],
     categoryValues: [0, 0, 0],
